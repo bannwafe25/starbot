2b69e6f04a1bfeb15f24c95ababa2288148c6e65
zpcode <zpkemem@gmail.com>
2026-08-05 13:34:37 +0800

    Update bingai.py

--- plugins/bingai.py
+++ plugins/bingai.py
@@ -1,18 +1,18 @@
-from command import bingai_cmd
+from command import bingimg_cmd
 from helpers import CMD
 
 IS_PRO = True
 
-__MODULES__ = "Bingai"
+__MODULES__ = "bingimg"
 __HELP__ = """<blockquote>Command Help **Bingai**</blockquote>
 <blockquote expandable>--**Basic Commands**--
 
     **You can generate image ai with Bingai from prompt command**
-        `{0}bingai` (prompt)</blockquote>
+        `{0}bingimg` (prompt)</blockquote>
 <b>   {1}</b>
 """
 
 
-@CMD.UBOT("bingai")
+@CMD.UBOT("bingimg")
 async def _(client, message):
-    return await bingai_cmd(client, message)
+    return await bingimg_cmd(client, message)
