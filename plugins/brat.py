71eab4dd5242481747ca16955937f8d944837f54
zpcode <zpkemem@gmail.com>
2026-08-06 16:14:00 +0800

    Update brat.py

--- plugins/brat.py
+++ plugins/brat.py
@@ -1,14 +1,14 @@
-from command import brat_cmd
+from command import brat_cmd, bratv2_cmd
 from helpers import CMD
 
 __MODULES__ = "Brat"
 __HELP__ = """<blockquote>Command Help **Brat**</blockquote>
 <blockquote expandable>--**Basic Commands**--
 
-    **You can make brat video using costum text**
-        `{0}vbrat` (text)
-    **You can make brat image using costum text**
-        `{0}brat` (text)</blockquote>
+    **You can make brat using costum text**
+        `{0}brat` (text)
+    **You can make brat v2 using costum text**
+        `{0}brat2` (text)</blockquote>
 <b>   {1}</b>
 """
 
@@ -16,3 +16,7 @@ __HELP__ = """<blockquote>Command Help **Brat**</blockquote>
 @CMD.UBOT("brat|vbrat")
 async def _(client, message):
     return await brat_cmd(client, message)
+
+@CMD.UBOT("brat2")
+async def _(client, message):
+    return await bratv2_cmd(client, message)
