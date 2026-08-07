b35abf725c296af8ecef79c0d0f536151a3f516d
zpcode <zpkemem@gmail.com>
2026-08-07 19:09:48 +0800

    Update blackbox.py

--- plugins/blackbox.py
+++ plugins/blackbox.py
@@ -3,11 +3,7 @@ __HELP__ = """<blockquote>Command Help **Blackbox**</blockquote>
 <blockquote expandable>--**Basic Commands**--
 
     **You can answer question to blackbox ai** 
-        `{0}blackbox` (question) 
-    **You can answer about image to blackbox ai** 
-        `{0}blackbox` (reply photo) (question) 
-    **You can generate image to blackbox ai** 
-        `{0}blackbox generate` (prompt)</blockquote>
+        `{0}ask` (question)</blockquote>
 <b>   {1}</b>
 """
 
@@ -17,6 +13,6 @@ from command import blackbox_cmd
 from helpers import CMD
 
 
-@CMD.UBOT("blackbox")
+@CMD.UBOT("ask")
 async def _(client, message):
     return await blackbox_cmd(client, message)
