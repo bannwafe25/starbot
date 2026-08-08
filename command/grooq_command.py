5f22bf6301afe0a1c71e044c9a37ea8c62f368dd
zp <inizulpii25@gmail.com>
2026-08-08 10:47:47 +0800

    Update grooq_command.py

--- command/grooq_command.py
+++ command/grooq_command.py
@@ -1,13 +1,18 @@
+import asyncio
+
 from groq import Groq
 from pyrogram.enums import ParseMode
 
 from config import GROQ_API_KEY
+from helpers.emoji import Emoji
 
 
 groq = Groq(api_key=GROQ_API_KEY)
 
 
 async def grooq_cmd(client, message):
+    em = Emoji(client)
+    await em.get()
 
     if not message.text:
         return
@@ -15,23 +20,39 @@ async def grooq_cmd(client, message):
     query = message.text.split(None, 1)
 
     if len(query) < 2:
-        return await message.reply(".ai pertanyaan")
+        return await message.reply(
+            f"{em.gagal} Gunakan:\n`.ai pertanyaan`"
+        )
 
-    await message.edit("🤖 Thinking...")
-
-    chat = groq.chat.completions.create(
-        model="llama-3.3-70b-versatile",
-        messages=[
-            {
-                "role": "user",
-                "content": query[1]
-            }
-        ]
+    progress = await message.edit(
+        f"{em.proses} Thinking..."
     )
 
-    result = chat.choices[0].message.content
+    try:
+        def ask_groq():
+            return groq.chat.completions.create(
+                model="llama-3.3-70b-versatile",
+                messages=[
+                    {
+                        "role": "user",
+                        "content": query[1]
+                    }
+                ]
+            )
 
-    await message.edit(
-        f"<blockquote>{result}</blockquote>",
-        parse_mode=ParseMode.HTML
-    )
+        chat = await asyncio.to_thread(
+            ask_groq
+        )
+
+        result = chat.choices[0].message.content
+
+        await progress.edit(
+            f"{em.sukses}\n\n"
+            f"<blockquote>{result}</blockquote>",
+            parse_mode=ParseMode.HTML
+        )
+
+    except Exception as e:
+        await progress.edit(
+            f"{em.gagal} Terjadi kesalahan:\n`{e}`"
+        )
